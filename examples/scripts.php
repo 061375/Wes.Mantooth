fc9790838f43917906cd2eaca3f08f25b90d32cc
Jeremy <j.heminger@061375.com>
2017-07-22 14:12:27 -0400

    google analytics

--- examples/scripts.php
+++ examples/scripts.php
@@ -41,5 +41,20 @@ if(true === $dev) {
         <script src="<?php echo $path; ?>../_/js/wes.mantooth.js"></script>
     <?php
 }
+// Track user interest
+if('' == $local) {
+    ?>
+    <script>
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+      
+        ga('create', 'UA-103050548-1', 'auto');
+        ga('send', 'pageview');
+      
+    </script>
+    <?php
+}
 ?>
     <script src="<?php echo $path; ?>../_/js/external/_syntaxhighlighter.js<?php echo $dev; ?>"></script>
