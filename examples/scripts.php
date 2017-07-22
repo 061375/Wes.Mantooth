1e9344baeb611044c205262c4876889ef14b4c98
Jeremy <j.heminger@061375.com>
2017-07-22 13:53:08 -0400

    modifications for live

--- examples/scripts.php
+++ examples/scripts.php
@@ -1,10 +1,20 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <?php
-$path = isset($path) ? $path : '../';
+
+// if this is my local machine the run Grunt auto-reload
+$local = '';
+if('192.168.1.157' == $_SERVER['REMOTE_ADDR'])
+$local = '<script src="http://192.168.1.154:35729/livereload.js"></script>';
+
+// allow for sub-pathing as neccesary
+$path = isset($_GET['path']) ? $path : '../';
+
+// development mode
 $dev = isset($_GET['dev']) ? true : false;
+
 if(true === $dev) {
     $dev = '?a='.strtotime('now');
-    $grunt = '<script src="http://192.168.1.154:35729/livereload.js"></script><script>$w.boolLog = true;console.log("Dumping window to show that no objects associated with Wesmantooth (aliased as $w) are stuck in root [this would be a memory leak]");console.log(window);</script>';
+    $grunt = $local.'<script>$w.boolLog = true;console.log("Dumping window to show that no objects associated with Wesmantooth (aliased as $w) are stuck in root [this would be a memory leak]");console.log(window);</script>';
     ?>
         <script src="<?php echo $path; ?>../_/components/js/_wes.mantooth.js<?php echo $dev; ?>"></script>
         <script src="<?php echo $path; ?>../_/components/js/canvas.js<?php echo $dev; ?>"></script>
@@ -18,7 +28,6 @@ if(true === $dev) {
         <script src="<?php echo $path; ?>../_/components/js/game.js<?php echo $dev; ?>"></script>
         <script src="<?php echo $path; ?>../_/components/js/shortys.js<?php echo $dev; ?>"></script>
         <script src="<?php echo $path; ?>../_/components/js/buttons.js<?php echo $dev; ?>"></script>
-
         <script src="<?php echo $path; ?>../_/js/external/js_memleak/memory_leak_checker.js"></script>
         <script>
             MemoryLeakChecker.checkLeaks($w);
